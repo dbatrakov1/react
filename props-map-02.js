98279066ba01af7f6beb36c29473abb1ad313ff0
Denis Batrakov <49515317+dbatrakov1@users.noreply.github.com>
2022-11-04 14:01:01 -0500

    Create props-map-02.js

---
+++ props-map-02.js
@@ -0,0 +1,21 @@
+import './App.css';
+import data from './data'
+import Card from './components/Card'
+
+function App() {
+  const cards = data.map(item => {
+    return(
+      <Card
+        item={item}
+      />
+    )
+  } )
+  console.log(data)
+  return (
+    <div className="App">
+      <div className='main--cards'>{cards}</div>
+    </div>
+  );
+}
+
+export default App;
