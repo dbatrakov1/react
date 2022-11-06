c79306f3eec1a16f2589014abfc59115e96b3046
Denis Batrakov <49515317+dbatrakov1@users.noreply.github.com>
2022-11-06 09:34:07 -0600

    Create App.js

---
+++ state-06( components got props and from this props inside the component  created state )/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import boxes from "./boxes"
+import  Box from "./Box"
+
+export default function App(props) {
+    const [squares, setSquares] =  React.useState(boxes)
+    const styles = {
+        backgroundColor: props.darkMode ? '#222222' : '#cccccc'
+    }
+    const  squareElements  = squares.map(square =>(
+        <Box key={square.id}  on={square.on} />
+    ))
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
+}
