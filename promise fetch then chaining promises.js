24ab323e21b53b28aac096541f912b8310ef7ef8
Denis Batrakov <49515317+dbatrakov1@users.noreply.github.com>
2023-04-04 19:58:53 -0500

    Update promise fetch then chaining promises.js

--- promise fetch then chaining promises.js
+++ promise fetch then chaining promises.js
@@ -9,3 +9,6 @@ fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-sto
   .then((data) => {
     console.log(data[0]);
   });
+// With a promise-based API, the asynchronous function starts the operation and returns a Promise object.
+// You can then attach handlers to this promise object, and these handlers will be executed when
+// the operation has succeeded or failed.
